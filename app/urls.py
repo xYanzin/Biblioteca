2fd61c32a93b34859740ee4032b1e98ccc882cf1
xYanzin <xyanzin12@gmail.com>
2024-05-06 17:07:24 -0300

    criate views

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import JsonResponse
-
+from genres.views import genre_view
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
+    path("admin/", admin.site.urls),
+    path("genres/", genre_view, name="genre-list"),
 ]
