c60152dae92f27cd145b5c74d3492754a1167437
xYanzin <xyanzin12@gmail.com>
2024-05-06 18:02:20 -0300

    detail-views

--- genres/views.py
+++ genres/views.py
@@ -1,11 +1,12 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_list_or_404
 from genres.models import Genre
 
 
 @csrf_exempt
-def genre_view(request):
+def genre_create_list_view(request):
     if request.method == "GET":
         genres = Genre.objects.all()
         data = [{"id": genre.id, "name": genre.name} for genre in genres]
@@ -19,3 +20,11 @@ def genre_view(request):
             {"id": new_genre.id, "name": new_genre.name},
             status=201,
         )
+
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_list_or_404(Genre, pk=pk)
+
+    data = {"id": genre.id, "name": genre.name}
+    return JsonResponse(data)
