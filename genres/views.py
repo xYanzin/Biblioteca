f8a7008b7efaa28a74463fd15767d7c7ee0707cd
xYanzin <xyanzin12@gmail.com>
2024-08-02 15:43:16 -0300

    CRUD completo

--- genres/views.py
+++ genres/views.py
@@ -1,7 +1,7 @@
 import json
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_list_or_404
+from django.views.decorators.csrf import csrf_exempt  # token padrão contra ataque.
+from django.shortcuts import get_object_or_404
 from genres.models import Genre
 
 
@@ -13,18 +13,34 @@ def genre_create_list_view(request):
         return JsonResponse(data, safe=False)
 
     elif request.method == "POST":
-        data = json.loads(request.body.decode("utf-8"))
+        data = json.loads(
+            request.body.decode("utf-8")
+        )  # serve para não quebrar quando o usuario inserir algum caractere
         new_genre = Genre(name=data["name"])
         new_genre.save()
         return JsonResponse(
             {"id": new_genre.id, "name": new_genre.name},
-            status=201,
+            status=201,  # status de que foi criado.
         )
 
 
 @csrf_exempt
 def genre_detail_view(request, pk):
-    genre = get_list_or_404(Genre, pk=pk)
+    genre = get_object_or_404(Genre, pk=pk)
 
-    data = {"id": genre.id, "name": genre.name}
-    return JsonResponse(data)
+    if request.method == "GET":
+        data = {"id": genre.id, "name": genre.name}
+        return JsonResponse(data)
+
+    elif request.method == "PUT":
+        data = json.loads(request.body.decode("utf-8"))
+        genre.name = data["name"]
+        genre.save()
+        return JsonResponse({"id": genre.id, "name": genre.name})
+
+    elif request.method == "DELETE":
+        genre.delete()
+        return JsonResponse(
+            {"message": "Gênero excluido com sucecsso."},
+            status=204,
+        )
