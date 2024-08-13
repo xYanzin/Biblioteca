b5c10773e91a5ea50cc81930b78201653861c734
xYanzin <xyanzin12@gmail.com>
2024-08-13 14:09:50 -0300

    criando CRUD actors

--- app/settings.py
+++ app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "genres",
     "rest_framework",
+    "actors",
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
+from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
 
 
 urlpatterns = [
@@ -10,5 +11,11 @@ urlpatterns = [
         "genres/<int:pk>/",
         GenreRetrieveUpdateDestroyView.as_view(),
         name="genre-detail-view",
+    ),
+    path("actors/", ActorCreateListView.as_view(), name="actor-create-list"),
+    path(
+        "actors/<int:pk>/",
+        ActorRetrieveUpdateDestroyView.as_view(),
+        name="actor-detail-view",
     ),
 ]
